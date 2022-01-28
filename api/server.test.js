f777a9e3c07001bb7040b00d80d458b7cb2d32ea
Michael Orvin <morvin1212@gmail.com>
2022-01-28 10:54:47 -0700

    register endpoints passing tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,6 @@
-// const request = require('supertest');
-// const server = require('../api/server');
-// const User = require('../api/users/users-model');
+const request = require("supertest");
+const server = require("../api/server");
+// const User = require("../api/users/users-model");
 const db = require("../data/dbConfig");
 // const Jokes = require('./jokes/jokes-data');
 
@@ -20,5 +20,30 @@ afterAll(async () => {
 describe("Testing on testing environment", () => {
   it("Check Environment", () => {
     expect(process.env.NODE_ENV).toBe("testing");
+  });
+});
+
+describe("registration endpoint", () => {
+  const user = { username: "harrypotter", password: "1234" };
+  const userOnly = { username: "harrypotter" };
+
+  it("[GET] /auth/register - username only, no password, returns 401", async () => {
+    let res = await request(server).post("/api/auth/register").send(userOnly);
+    expect(res.status).toBe(401);
+  });
+
+  it("[GET] /auth/register - credentials complete, returns 201", async () => {
+    let res = await request(server).post("/api/auth/register").send(user);
+    expect(res.status).toBe(201);
+  });
+
+  it("[GET] /auth/register - after 201, confirmation in the db", async () => {
+    const [userExists] = await db("users").where("username", user.username);
+    expect(userExists).toMatchObject({ username: user.username });
+  });
+
+  it("[GET] /auth/register - existing user, returns 422", async () => {
+    let res = await request(server).post("/api/auth/register").send(user);
+    expect(res.status).toBe(422);
   });
 });
